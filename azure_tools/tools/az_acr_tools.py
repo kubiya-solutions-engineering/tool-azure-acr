9241e74377c3961280c1bb5a80c53943eeb40ec5
Chris Mills <chris.mills@kubiya.ai>
2024-10-02 15:56:19 -0500

    updated list tags tool

--- azure_tools/tools/az_acr_tools.py
+++ azure_tools/tools/az_acr_tools.py
@@ -27,8 +27,8 @@ azure_acr_tool = AzureACRCliTool(
     ],
 )
 
-azure_show_tags_older_than_date = AzureACRCliTool(
-    name="azure_show_tags_older_than_date",
+list_tags = AzureACRCliTool(
+    name="list_tags",
     description=("""
         Shows tags of a provided Azure ACR registry and repository that are older than a specified
         date."
@@ -36,32 +36,7 @@ azure_show_tags_older_than_date = AzureACRCliTool(
     content="""
             export NEW_DATE=$(date -d "{{ .num_days}} days ago" +%Y-%m-%d)
             az acr repository show-tags -n {{ .registry}} --repository {{ .repository}} --detail \
-            --query "[?lastUpdateTime<'$NEW_DATE'].{Name:name, LastUpdate:lastUpdateTime}" \
-            --orderby time_asc""",
-    args=[
-        Arg(name="registry", 
-            type="str", 
-            description=("The Azure ACR registry."), 
-            required=True),
-        Arg(name="repository",  
-            type="str", 
-            description=("The Azure ACR repository within the chosen registry."), 
-            required=True),
-        Arg(name="num_days",
-            type="int",
-            description=("The number of days indicating age of tags. For example, `today` would be `0` and `two days ago` would be `2`."),
-            required=True),
-    ],
-)
-
-azure_delete_tags_older_than_date = AzureACRCliTool(
-    name="azure_delete_tags_older_than_date",
-    description=("Deletes tags of a provided Azure ACR registry and repository that are older than a specified date."),
-    content="""
-            export NEW_DATE=$(date -d "{{ .num_days}} days ago" +%Y-%m-%d)
-            for tag in $(az acr repository show-tags -n {{ .registry}} --repository {{ .repository}} --query "[?lastUpdateTime<'$NEW_DATE' && starts_with(@, '{{ .filter }}')].name" -o tsv); do
-                az acr repository delete -n {{ .registry}} --image {{ .repository }}:$tag --yes
-            done
+            --query "[?lastUpdateTime<'$NEW_DATE' && starts_with(name, '{{ .starts_with}}')].{Name:name, LastUpdate:lastUpdateTime}" 
             """,
     args=[
         Arg(name="registry", 
@@ -72,16 +47,47 @@ azure_delete_tags_older_than_date = AzureACRCliTool(
             type="str", 
             description=("The Azure ACR repository within the chosen registry."), 
             required=True),
-        Arg(name="filter",
-            type="str",
-            description=("A string to filter tags that start with this value."),
-            required=False),
         Arg(name="num_days",
             type="int",
             description=("The number of days indicating age of tags. For example, `today` would be `0` and `two days ago` would be `2`."),
-            required=True),
+            default=0,
+            required=False),
+        Arg(name="starts_with",
+            type="str",
+            description=("A string to filter tags that start with this value."),
+            default='',
+            required=False),
     ],
 )
+
+#azure_delete_tags_older_than_date = AzureACRCliTool(
+#    name="azure_delete_tags_older_than_date",
+#    description=("Deletes tags of a provided Azure ACR registry and repository that are older than a specified date."),
+#    content="""
+#            export NEW_DATE=$(date -d "{{ .num_days}} days ago" +%Y-%m-%d)
+#            for tag in $(az acr repository show-tags -n {{ .registry}} --repository {{ .repository}} --query "[?lastUpdateTime<'$NEW_DATE' && starts_with(@, '{{ .filter }}')].name" -o tsv); do
+#                az acr repository delete -n {{ .registry}} --image {{ .repository }}:$tag --yes
+#            done
+#            """,
+#    args=[
+#        Arg(name="registry", 
+#            type="str", 
+#            description=("The Azure ACR registry."), 
+#            required=True),
+#        Arg(name="repository",  
+#            type="str", 
+#            description=("The Azure ACR repository within the chosen registry."), 
+#            required=True),
+#        Arg(name="filter",
+#            type="str",
+#            description=("A string to filter tags that start with this value."),
+#            required=False),
+#        Arg(name="num_days",
+#            type="int",
+#            description=("The number of days indicating age of tags. For example, `today` would be `0` and `two days ago` would be `2`."),
+#            required=True),
+#    ],
+#)
 
 
 tool_registry.register("Azure Container Repository", azure_show_tags_older_than_date)
