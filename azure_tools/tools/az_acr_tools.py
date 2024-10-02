08026ebb43a53e90122646c27f91828978764ef9
michagonzo77 <caleb.g@nylas.com>
2024-10-02 11:47:29 -0700

    removing 10 tag limit and adding delete tool

--- azure_tools/tools/az_acr_tools.py
+++ azure_tools/tools/az_acr_tools.py
@@ -37,7 +37,7 @@ azure_show_tags_older_than_date = AzureACRCliTool(
             export NEW_DATE=$(date -d "{{ .num_days}} days ago" +%Y-%m-%d)
             az acr repository show-tags -n {{ .registry}} --repository {{ .repository}} --detail \
             --query "[?lastUpdateTime<'$NEW_DATE'].{Name:name, LastUpdate:lastUpdateTime}" \
-            --orderby time_asc --top 10""",
+            --orderby time_asc""",
     args=[
         Arg(name="registry", 
             type="str", 
@@ -54,5 +54,36 @@ azure_show_tags_older_than_date = AzureACRCliTool(
     ],
 )
 
+azure_delete_tags_older_than_date = AzureACRCliTool(
+    name="azure_delete_tags_older_than_date",
+    description=("Deletes tags of a provided Azure ACR registry and repository that are older than a specified date."),
+    content="""
+            export NEW_DATE=$(date -d "{{ .num_days}} days ago" +%Y-%m-%d)
+            for tag in $(az acr repository show-tags -n {{ .registry}} --repository {{ .repository}} --query "[?lastUpdateTime<'$NEW_DATE' && starts_with(@, '{{ .filter }}')].name" -o tsv); do
+                az acr repository delete -n {{ .registry}} --repository {{ .repository}} --tag $tag
+            done
+            """,
+    args=[
+        Arg(name="registry", 
+            type="str", 
+            description=("The Azure ACR registry."), 
+            required=True),
+        Arg(name="repository",  
+            type="str", 
+            description=("The Azure ACR repository within the chosen registry."), 
+            required=True),
+        Arg(name="filter",
+            type="str",
+            description=("A string to filter tags that start with this value."),
+            required=False),
+        Arg(name="num_days",
+            type="int",
+            description=("The number of days indicating age of tags. For example, `today` would be `0` and `two days ago` would be `2`."),
+            required=True),
+    ],
+)
+
+
 tool_registry.register("Azure Container Repository", azure_show_tags_older_than_date)
 tool_registry.register("Azure Container Repository", azure_acr_tool)
+tool_registry.register("Azure Container Repository", azure_delete_tags_older_than_date)
